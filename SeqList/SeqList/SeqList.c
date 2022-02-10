6c4ab5476e7c3383bd483973b8d47065f9a22650
lrf <465197183@qq.com>
2022-02-10 22:49:54 +0800

    增加部分功能

--- SeqList/SeqList/SeqList.c
+++ SeqList/SeqList/SeqList.c
@@ -48,40 +48,98 @@ void SeqListInit(SL* ps)
 void SeqListPushBack(SL* ps, SLDataType x)
 {
 	//没有空间 空间不足就扩容
-	SeqListCheckCapacity(ps);
-	ps->a[ps->size] = x;//空间足够
-	ps->size++;
+	//1.
+	//SeqListCheckCapacity(ps);
+	//ps->a[ps->size] = x;//空间足够
+	//ps->size++;
+	//2.调用插入函数
+	SeqListInsert(ps, ps->size, x);
 }
 
 void SeqListPopBack(SL* ps)
 {
+	//两种处理顺序表为空的方式均可
 	//if (ps->size > 0)
 	//{
 	//	ps->size--;
 	//}
-	assert(ps->size > 0);
-	//两种方式均可
+	//(1).手动实现
+	//assert(ps->size > 0);
+	//ps->size--;
+	//(2).调用删除函数
+	SeqListErase(ps, ps->size - 1);
+	
 }
 
 void SeqListPushFront(SL* ps, SLDataType x)
 {
 	SeqListCheckCapacity(ps);
 	//挪动数据
-	int end = ps->size - 1;
-	while (end >= 0)
-	{
-		ps->a[end + 1] = ps->a[end];
-		end--;
-	}
-	ps->a[0] = x;
-	ps->size++;
+	//1.手动实现
+	//int end = ps->size - 1;
+	//while (end >= 0)
+	//{
+	//	ps->a[end + 1] = ps->a[end];
+	//	end--;
+	//}
+	//ps->a[0] = x;
+	//ps->size++;
+	//2.调用插入函数
+	SeqListInsert(ps, 0, x);
 }
 
 void SeqListPopFront(SL* ps)
 {
-	assert(ps->size > 0);
-	int begin = 1;
-	while (begin < ps->size)
+	//1.手动实现
+	//assert(ps->size > 0);
+	//int begin = 1;
+	//while (begin < ps->size)
+	//{
+	//	ps->a[begin - 1] = ps->a[begin];
+	//	begin++;
+	//}
+	//ps->size--;
+	//2.调用实现的删除函数
+	SeqListErase(ps, 0);
+}
+
+
+int SeqListFind(SL* ps, SLDataType x)
+{
+	for (int i = 0; i < ps->size; i++)
+	{
+		if (ps->a[i] == x)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void SeqListInsert(SL* ps, int pos, SLDataType x)
+{
+	//if (pos > ps->size || pos < 0)
+	//{
+	//	printf("pos invalid\n");
+	//	return;
+	//}
+	assert(pos <= ps->size&&pos >= 0);
+	SeqListCheckCapacity(ps);
+	int end = ps->size - 1;
+	while (end >= pos)
+	{
+		ps->a[end + 1] = ps->a[end];
+		end--;
+	}
+	ps->a[pos] = x;
+	ps->size++;
+}
+
+void SeqListErase(SL* ps, int pos)
+{
+	assert(pos >= 0 && pos < ps->size);
+	int begin = pos + 1;
+	while (begin <= ps->size - 1)
 	{
 		ps->a[begin - 1] = ps->a[begin];
 		begin++;
--- SeqList/SeqList/Test.c
+++ SeqList/SeqList/Test.c
@@ -1,5 +1,28 @@
 #include"SeqList.h"
 
+void TestSeqList3()
+{
+	SL sl;
+	SeqListInit(&sl);
+	SeqListPushBack(&sl, 1);
+	SeqListPushBack(&sl, 2);
+	SeqListPushBack(&sl, 3);
+	SeqListPushBack(&sl, 4);
+	SeqListPushBack(&sl, 5);
+	SeqListInsert(&sl, 0, 1);
+	SeqListPrint(&sl);
+	SeqListInsert(&sl, 1, 2);
+	SeqListPrint(&sl);
+	SeqListInsert(&sl, SeqListFind(&sl,5), 2);
+	SeqListPrint(&sl);
+	SeqListErase(&sl, 1);
+	SeqListPopBack(&sl);
+	SeqListPrint(&sl);
+	SeqListPopFront(&sl);
+	SeqListPrint(&sl);
+	SeqListDestory(&sl);
+}
+
 void TestSeqList2()
 {
 	SL sl;
@@ -39,16 +62,26 @@ void TestSeqList1()
 	SeqListPopBack(&sl);
 	SeqListPopBack(&sl);
 	SeqListPopBack(&sl);
-	SeqListPrint(&sl);
 
-	SeqListPopBack(&sl, 10);
-	SeqListPopBack(&sl, 20);
+
+	SeqListPrint(&sl);
+	
 	SeqListDestory(&sl);
+}
+void menu()
+{
+	printf("***************************************");
+	printf("*             请选择你的操作           *");
+	printf("*             1.头插                               *");
+	printf("*             2.头删                          *");
+	printf("*                                     *");
+	printf("***************************************");
 }
 
 int main()
 {
 	//TestSeqList1();
-	TestSeqList2();
+	//TestSeqList2();
+	TestSeqList3();
 	return 0;
 }
