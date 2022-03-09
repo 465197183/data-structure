80f952dc29e3fd7dc9198a9164a197149a267ed5
lrf <465197183@qq.com>
2022-03-09 23:03:52 +0800

    小改动

---
+++ Queue/Queue/Queue.c
@@ -0,0 +1,45 @@
+#include"Queue.h"
+
+
+void QueueInit(Queue* pq)
+{
+	assert(pq);
+	pq->head = NULL;
+	pq->tail = NULL;
+}
+
+void QueueDestroy(Queue* pq)
+{
+	assert(pq);
+	QueueNode* cur = pq;
+	while (cur)
+	{
+		QueueNode* next = cur->next;
+		free(cur);
+		cur = next->next;
+	}
+	pq->head = pq->tail = NULL;
+}
+
+void QueuePush(Queue* pq, QDataType x)
+{
+	assert(pq);
+	QueueNode* newnode = (QueueNode*)malloc(sizeof(QueueNode));
+	if (newnode == NULL)
+	{
+		perror("QueuePush::");
+		exit(-1);
+	}
+	newnode->data = x;
+	newnode->next = NULL;
+	if (pq->head == NULL)
+	{
+		pq->head = pq->tail = newnode;
+	}
+	else
+	{
+		pq->tail->next = newnode;
+		pq->tail = newnode;
+	}
+}
+
---
+++ Queue/Queue/Test.c
@@ -0,0 +1,19 @@
+#include"Queue.h"
+
+void TestQueue1()
+{
+	Queue q;
+	QueueInit(&q);
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
+	QueuePush(&q, 5);
+}
+
+int main()
+{
+	TestQueue1();
+
+	return 0;
+}
