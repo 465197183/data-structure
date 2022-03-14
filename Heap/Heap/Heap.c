515c9b8caf56830573dfffcef581ba3b6ab8e13c
lrf <465197183@qq.com>
2022-03-14 22:59:48 +0800

    TopK问题

--- Heap/Heap/Heap.c
+++ Heap/Heap/Heap.c
@@ -7,38 +7,44 @@ void HeapInit(HP* hp)
 	hp->size = hp->capacity = 0;
 }
 
-//void AdjustUp(int* a,int child)
-//{
-//	assert(a);
-//	int parent = (child - 1) / 2;
-//	while (child > 0) 
-//	{
-//		if (a[child] > a[parent])
-//		{
-//			HeapDataType tmp = a[child];
-//			a[child] = a[parent];
-//			a[parent] = tmp;
-//
-//			child = parent;
-//			parent = (child - 1) / 2;
-//		}
-//		else
-//		{
-//			break;
-//		}
-//	}
-//}
-void AdjustUp(HeapDataType* a, int child)
+HeapDataType HeapTop(HP* hp)
+{
+	assert(hp);
+	assert(!HeapEmpty(hp));
+	return hp->a[0];
+}
+
+
+void AdjustUp(int* a,int child)
 {
 	assert(a);
 	int parent = (child - 1) / 2;
-	while (a[child] > a[parent]) 
+	while (child > 0) 
 	{
-		Swap(&a[child], &a[parent]);
-		child = parent;
-		parent = (child - 1) / 2;
+		if (a[child] < a[parent])
+		{
+			Swap(&a[child], &a[parent]);
+
+			child = parent;
+			parent = (child - 1) / 2;
+		}
+		else
+		{
+			break;
+		}
 	}
 }
+//void AdjustUp(HeapDataType* a, int child)
+//{
+//	assert(a);
+//	int parent = (child - 1) / 2;
+//	while (a[child] > a[parent]) 
+//	{
+//		Swap(&a[child], &a[parent]);
+//		child = parent;
+//		parent = (child - 1) / 2;
+//	}
+//}
 
 void AdjustDown(HeapDataType* a, int n, int parent)
 {
@@ -110,7 +116,7 @@ int HeapSize(HP* hp)
 void HeapPop(HP* hp)
 {
 	assert(hp);
-	assert(HeapEmpty(hp));
+	assert(!HeapEmpty(hp));
 	Swap(&hp->a[0], &hp->a[hp->size - 1]);
 	hp->size--;
 	AdjustDown(hp->a, hp->size, 0);
--- Heap/Heap/Test.c
+++ Heap/Heap/Test.c
@@ -1,14 +1,76 @@
 #include"Heap.h"
 
-int main()
+void PrintTopK(int* a, int n, int k)
 {
-	int a[] = {15,18,19,25,28,34,65,49,27,37,10 };
 	HP hp;
 	HeapInit(&hp);
-	for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+	//创建k个数的小堆
+	for (int i = 0; i < k; i++)
 	{
-		HeapPush(&hp,a[i]);
+		HeapPush(&hp, a[i]);
+	}
+	//剩下的N-K个数与堆顶数据比较，若大于堆顶数据这替换该数据进堆
+	for (int i = k; i < n; i++)
+	{
+		if (a[i] > HeapTop(&hp))
+		{
+			//(1)
+			//HeapPop(&hp);
+			//HeapPush(&hp, a[i]);
+			//(2)
+			hp.a[0] = a[i];
+			AdjustDown(hp.a, hp.size, 0);
+		}
 	}
 	HeapPrint(&hp);
+	//HeapDestroy(&hp);
+}
+
+void TestTopk()
+{
+	int n = 10000;
+	int* a = (int*)malloc(sizeof(int) * n);
+	srand(time(0));
+	for (size_t i = 0; i < n; ++i)
+	{
+		a[i] = rand() % 1000000;
+	}
+	a[5] = 1000000 + 1;
+	a[1231] = 1000000 + 2;
+	a[531] = 1000000 + 3;
+	a[5121] = 1000000 + 4;
+	a[115] = 1000000 + 5;
+	a[2335] = 1000000 + 6;
+	a[9999] = 1000000 + 7;
+	a[76] = 1000000 + 8;
+	a[423] = 1000000 + 9;
+	a[3144] = 1000000 + 10;
+	PrintTopK(a, n, 10);
+}
+
+
+int main()
+{
+	TestTopk();
 	return 0;
 }
+
+//int main()
+//{
+//	int a[] = {70,56,30,25,15,10,75 };
+//	HP hp;
+//	HeapInit(&hp);
+//	for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+//	{
+//		HeapPush(&hp,a[i]);
+//		HeapEmpty(&hp);
+//	}
+//	HeapPrint(&hp);
+//	HeapPop(&hp);
+//	HeapPrint(&hp);
+//	HeapPop(&hp);
+//	HeapPrint(&hp);
+//	HeapPop(&hp);
+//	HeapPrint(&hp);
+//	return 0;
+//}
